626e4617f13091abc14f0d3e3b47a5a9b30efe7e
Arjun Das <adshaishab212@gmail.com>
2026-05-11 11:02:10 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,48 @@
+#1
+
+name = input('Enter your name: ')
+
+print("My name is " + name)
+
+#2
+
+batch_no = input("Enter your batch no: ")
+
+print("So, batch no is " + batch_no)
+
+# 3
+
+name = "Mijan"
+id_no = 5
+balance = 1000
+char1 = 'a'
+PI = 3.14159
+
+print(name, id_no, balance, char1, PI)
+
+# 4
+
+StudentAge = 8
+
+# 5
+
+a = b = c = 100
+print(a, b, c)
+
+#6
+
+_floatValue = input("Enter a float value: ")
+
+print(_floatValue)
+
+#7
+
+a = "Welcome to Django for web and Ai"
+
+print(a.upper())
+
+#8
+
+b = "       Python"
+
+print(b.strip())
